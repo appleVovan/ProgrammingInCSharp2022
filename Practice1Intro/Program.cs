83796d8535aa82f636296155d3f63b657f4b11c0
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-25 20:21:02 +0200

    Reference type by value with property changing

--- Practice1Intro/Program.cs
+++ Practice1Intro/Program.cs
@@ -5,16 +5,37 @@ namespace KMA.ProgrammingInCSharp2022.Practice1Intro
     class Program
     {
         #region ValueType
-        static void Main(string[] args)
+        //static void Main(string[] args)
+        //{
+        //    MyMethodInt(out int myInt);
+        //    Console.WriteLine(myInt);
+        //}
+
+        //public static void MyMethodInt(out int myInt)
+        //{
+        //    myInt = 6;
+        //    Console.WriteLine(myInt);
+        //}
+        #endregion
+
+        #region ReferenceType
+        public class MyClass
         {
-            MyMethodInt(out int myInt);
-            Console.WriteLine(myInt);
+            public int MyProperty { get; set; }
         }
 
-        public static void MyMethodInt(out int myInt)
+        static void Main(string[] args)
         {
-            myInt = 6;
-            Console.WriteLine(myInt);
+            MyClass myObject = new MyClass();
+            myObject.MyProperty = 5;
+            MyMethodMyObject(myObject);
+            Console.WriteLine(myObject.MyProperty);
+        }
+
+        public static void MyMethodMyObject(MyClass myObject)
+        {
+            myObject.MyProperty = 6;
+            Console.WriteLine(myObject.MyProperty);
         }
         #endregion
     }
