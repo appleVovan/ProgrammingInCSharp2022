1d7c5f1384324b278f499141705a0ad3f70ff2b2
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-25 15:35:37 +0200

    Method getters and setterrs were removed

--- Practice1Intro/Dog.cs
+++ Practice1Intro/Dog.cs
@@ -16,21 +16,9 @@
             } 
         }
 
-        public int Get_Age()
-        {
-            return age;
-        }
-
-        public void Set_Age(int val)
-        {
-            this.age = val;
-        }
 
         public void MyMethod()
         {
-            int val = Get_Age();
-            Set_Age(5);
-
             int val1 = Age;
             Age = 5;
         }
