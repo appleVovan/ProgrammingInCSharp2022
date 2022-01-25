211f6f42ce8289c0fac8629dea71411e12ba840e
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-25 21:17:33 +0200

    framework version and namespaces were changed

--- Practice2LoginWindow/App.xaml.cs
+++ Practice2LoginWindow/App.xaml.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
-namespace Practice2LoginWindow
+namespace KMA.ProgrammingInCSharp2022.Practice2LoginWindow
 {
     /// <summary>
     /// Interaction logic for App.xaml
