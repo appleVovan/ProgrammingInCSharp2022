4511de91c0d0284e31e35153f3045e75eb0c7202
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-25 20:27:28 +0200

    Inheritance and polymorphism example

---
+++ Practice1Intro/Inheritance.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace KMA.ProgrammingInCSharp2022.Practice1Intro
+{
+    public class BaseClass
+    {
+        public void NotVirtual()
+        {
+            Console.WriteLine("NotVirtual BaseClass");
+        }
+
+        public virtual void Virtual()
+        {
+            Console.WriteLine("Virtual BaseClass");
+        }
+    }
+
+
+    public class DerivedClassOverride : BaseClass
+    {
+
+        public override void Virtual()
+        {
+            Console.WriteLine("Virtual DerivedClassOverride");
+        }
+    }
+
+    public class DerivedClassHide : BaseClass
+    {
+        public void NotVirtual()
+        {
+            Console.WriteLine("NotVirtual DerivedClassHide");
+        }
+
+        public void Virtual()
+        {
+            Console.WriteLine("Virtual DerivedClassHide");
+        }
+    }
+
+    public class DerivedClassHideNew : BaseClass
+    {
+        public new void NotVirtual()
+        {
+            Console.WriteLine("NotVirtual DerivedClassHideNew");
+        }
+
+        public new void Virtual()
+        {
+            Console.WriteLine("Virtual DerivedClassHideNew");
+        }
+    }
+}
--- Practice1Intro/Program.cs
+++ Practice1Intro/Program.cs
@@ -24,20 +24,59 @@ namespace KMA.ProgrammingInCSharp2022.Practice1Intro
             public int MyProperty { get; set; }
         }
 
+        //static void Main(string[] args)
+        //{
+        //    MyClass myObject = new MyClass();
+        //    myObject.MyProperty = 5;
+        //    MyMethodMyObject(ref myObject);
+        //    Console.WriteLine(myObject.MyProperty);
+        //}
+
+        //public static void MyMethodMyObject(ref MyClass myObject)
+        //{
+        //    myObject = new MyClass();
+        //    myObject.MyProperty = 6;
+        //    Console.WriteLine(myObject.MyProperty);
+        //}
+        #endregion
+
         static void Main(string[] args)
         {
-            MyClass myObject = new MyClass();
-            myObject.MyProperty = 5;
-            MyMethodMyObject(ref myObject);
-            Console.WriteLine(myObject.MyProperty);
-        }
+            BaseClass baseObject = new BaseClass();
+            DerivedClassOverride derivedOverrideObject = new DerivedClassOverride();
+            DerivedClassHide derivedHideObject = new DerivedClassHide();
+            DerivedClassHideNew derivedHideNewObject = new DerivedClassHideNew();
 
-        public static void MyMethodMyObject(ref MyClass myObject)
-        {
-            myObject = new MyClass();
-            myObject.MyProperty = 6;
-            Console.WriteLine(myObject.MyProperty);
+            Console.WriteLine("Call for baseObject {0}");
+            baseObject.NotVirtual();
+            baseObject.Virtual();
+            Console.WriteLine("Call for derivedVirtualObject {0}");
+            derivedOverrideObject.NotVirtual();
+            derivedOverrideObject.Virtual();
+            Console.WriteLine("Call for derivedObject {0}");
+            derivedHideObject.NotVirtual();
+            derivedHideObject.Virtual();
+            Console.WriteLine("Call for derivedNewObject {0}");
+            derivedHideNewObject.NotVirtual();
+            derivedHideNewObject.Virtual();
+            Console.ReadKey();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Call for baseObject {0}");
+            baseObject.NotVirtual();
+            baseObject.Virtual();
+            Console.WriteLine("Call for casted derivedVirtualObject {0}");
+            ((BaseClass)derivedOverrideObject).NotVirtual();
+            ((BaseClass)derivedOverrideObject).Virtual();
+            Console.WriteLine("Call for casted derivedObject {0}");
+            ((BaseClass)derivedHideObject).NotVirtual();
+            ((BaseClass)derivedHideObject).Virtual();
+            Console.WriteLine("Call for casted derivedNewObject {0}");
+            ((BaseClass)derivedHideNewObject).NotVirtual();
+            ((BaseClass)derivedHideNewObject).Virtual();
+            Console.ReadKey();
         }
-        #endregion
     }
 }
