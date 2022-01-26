1889e1263605091b77e4bfd7041ec97730dcc490
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-26 18:43:02 +0200

    Method implementation was added

--- Practice2LoginWindow/SignInWindow.xaml.cs
+++ Practice2LoginWindow/SignInWindow.xaml.cs
@@ -26,17 +26,31 @@ namespace KMA.ProgrammingInCSharp2022.Practice2LoginWindow
 
         private void BSignIn_Click(object sender, RoutedEventArgs e)
         {
-
+            if (String.IsNullOrWhiteSpace(TBLogin.Text) ||
+                String.IsNullOrWhiteSpace(PBPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"Login successful for user {TBLogin.Text}");
+            Close();
         }
 
         private void BSignUp_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if (String.IsNullOrWhiteSpace(TBLogin.Text) ||
+                String.IsNullOrWhiteSpace(PBPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"User with name {TBLogin.Text} was created");
+            Close();
         }
 
         private void BCancel_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            Environment.Exit(0);
         }
     }
 }
