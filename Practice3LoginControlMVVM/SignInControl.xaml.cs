02e36426df00dc7b9356f3916a8e819c3eada0a1
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-31 18:12:16 +0200

    Practice 3 prject was added (Practice 2 Clone)

---
+++ Practice3LoginControlMVVM/SignInControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM
+{
+    /// <summary>
+    /// Interaction logic for SignInControl.xaml
+    /// </summary>
+    public partial class SignInControl : UserControl
+    {
+        public SignInControl()
+        {
+            InitializeComponent();
+        }
+
+        private void BSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TBLogin.Text) ||
+                String.IsNullOrWhiteSpace(PBPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"Login successful for user {TBLogin.Text}");
+        }
+
+        private void BSignUp_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TBLogin.Text) ||
+                String.IsNullOrWhiteSpace(PBPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"User with name {TBLogin.Text} was created");
+        }
+
+        private void BCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Practice3LoginControlMVVM/SignInWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+
+namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM
+{
+    /// <summary>
+    /// Interaction logic for SignInWindow.xaml
+    /// </summary>
+    public partial class SignInWindow : Window
+    {
+        public SignInWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TBLogin.Text) ||
+                String.IsNullOrWhiteSpace(PBPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"Login successful for user {TBLogin.Text}");
+            Close();
+        }
+
+        private void BSignUp_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(TBLogin.Text) ||
+                String.IsNullOrWhiteSpace(PBPassword.Password))
+            {
+                MessageBox.Show("Login or password is empty!");
+                return;
+            }
+            MessageBox.Show($"User with name {TBLogin.Text} was created");
+            Close();
+        }
+
+        private void BCancel_OnClick(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
