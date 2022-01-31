1195411e9cfa4a48be080c6a23bf22fa2f603d57
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-31 19:34:17 +0200

    code was added to SignInViewModel

--- Practice3LoginControlMVVM/ViewModels/SignInViewModel.cs
+++ Practice3LoginControlMVVM/ViewModels/SignInViewModel.cs
@@ -1,6 +1,37 @@
-﻿namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM.ViewModels
+﻿using KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM.Models;
+
+namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM.ViewModels
 {
     class SignInViewModel
     {
+        #region Fields
+        private UserCandidate _user = new UserCandidate();
+        #endregion
+
+        #region Properties
+        public string Login
+        {
+            get
+            {
+                return _user.Login;
+            }
+            set
+            {
+                _user.Login = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _user.Password;
+            }
+            set
+            {
+                _user.Password = value;
+            }
+        }
+        #endregion
     }
 }
