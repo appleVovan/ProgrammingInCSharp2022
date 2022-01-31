d55edd8514c0635da355f14719d24238556947fa
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-31 18:59:07 +0200

    code was fixed to allow compilation

--- Practice3LoginControlMVVM/SignInControl.xaml.cs
+++ Practice3LoginControlMVVM/SignInControl.xaml.cs
@@ -16,24 +16,24 @@ namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM
 
         private void BSignIn_Click(object sender, RoutedEventArgs e)
         {
-            if (String.IsNullOrWhiteSpace(TbLogin.Text) ||
+            if (String.IsNullOrWhiteSpace(TbLogin.TbValue.Text) ||
                 String.IsNullOrWhiteSpace(PBPassword.Password))
             {
                 MessageBox.Show("Login or password is empty!");
                 return;
             }
-            MessageBox.Show($"Login successful for user {TbLogin.Text}");
+            MessageBox.Show($"Login successful for user {TbLogin.TbValue.Text}");
         }
 
         private void BSignUp_OnClick(object sender, RoutedEventArgs e)
         {
-            if (String.IsNullOrWhiteSpace(TbLogin.Text) ||
+            if (String.IsNullOrWhiteSpace(TbLogin.TbValue.Text) ||
                 String.IsNullOrWhiteSpace(PBPassword.Password))
             {
                 MessageBox.Show("Login or password is empty!");
                 return;
             }
-            MessageBox.Show($"User with name {TbLogin.Text} was created");
+            MessageBox.Show($"User with name {TbLogin.TbValue.Text} was created");
         }
 
         private void BCancel_OnClick(object sender, RoutedEventArgs e)
