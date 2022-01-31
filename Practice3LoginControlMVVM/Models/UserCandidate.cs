c980cdc22b671ab552f9eea8e2f016c6af965528
appleVovan <yablonskyi.v.reg@gmail.com>
2022-01-31 19:31:46 +0200

    login and password were added to UserCandidate

--- Practice3LoginControlMVVM/Models/UserCandidate.cs
+++ Practice3LoginControlMVVM/Models/UserCandidate.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM.Models
+﻿namespace KMA.ProgrammingInCSharp2022.Practice3LoginControlMVVM.Models
 {
     class UserCandidate
     {
+        private string _login;
+        private string _password;
+        
+        public string Login
+        {
+            get
+            {
+                return _login;
+            }
+            set
+            {
+                _login = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            }
+        }
     }
 }
