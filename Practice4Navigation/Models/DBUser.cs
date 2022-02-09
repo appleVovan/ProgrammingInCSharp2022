9e1139dd0b8dcaf8a482b6a2a2518bdc96573356
appleVovan <yablonskyi.v.reg@gmail.com>
2022-02-09 11:01:42 +0200

    DBUser was added

---
+++ Practice4Navigation/Models/DBUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.Models
+{
+    class DBUser
+    {
+        public DBUser(string firstName, string lastName, string email, string login, string password)
+        {
+            Guid = Guid.NewGuid();
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Login = login;
+            Password = password;
+        }
+
+        public Guid Guid { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string Email { get; }
+        public string Login { get; }
+        public string Password { get; }
+
+
+    }
+}
