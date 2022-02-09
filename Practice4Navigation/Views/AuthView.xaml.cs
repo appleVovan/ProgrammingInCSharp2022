8d55c4442de4514ab5f777cca7273ae8469cee8e
appleVovan <yablonskyi.v.reg@gmail.com>
2022-02-09 15:16:49 +0200

    AuthView was added

---
+++ Practice4Navigation/Views/AuthView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Controls;
+using KMA.ProgrammingInCSharp2022.Practice4Navigation.ViewModels;
+
+namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.Views
+{
+    /// <summary>
+    /// Interaction logic for AuthView.xaml
+    /// </summary>
+    public partial class AuthView : UserControl
+    {
+        public AuthView()
+        {
+            InitializeComponent();
+            DataContext = new AuthViewModel();
+        }
+    }
+}
