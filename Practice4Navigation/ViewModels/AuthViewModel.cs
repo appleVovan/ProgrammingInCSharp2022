2073f021813d35b3c8f9960b4772f50ce8d38075
appleVovan <yablonskyi.v.reg@gmail.com>
2022-02-09 14:55:42 +0200

    Navigation Method was implemented

--- Practice4Navigation/ViewModels/AuthViewModel.cs
+++ Practice4Navigation/ViewModels/AuthViewModel.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KMA.ProgrammingInCSharp2022.Practice4Navigation.Navigation;
 
 namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.ViewModels
 {
     class AuthViewModel
     {
-        private List<object> _viewModels = new List<object>();
+        private List<IAuthNavigatable> _viewModels = new List<IAuthNavigatable>();
 
-        public object CurrentViewModel
+        public IAuthNavigatable CurrentViewModel
         {
             get;
             private set;
@@ -23,10 +24,28 @@ namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.ViewModels
 
         internal void Navigate(int type)
         {
-            if (CurrentViewModel != null && CurrentViewModel)
-            {
+            if (CurrentViewModel != null && CurrentViewModel.ViewType == type)
+                return;
 
+            IAuthNavigatable viewModel = _viewModels.FirstOrDefault(viewModel => viewModel.ViewType == type);
+
+            if (viewModel == null)
+            {
+                if (type == 1)
+                    viewModel = new SignInViewModel(() => Navigate(2), ExitNavigation);
+                else if (type == 2)
+                    viewModel = new SignUpViewModel(() => Navigate(1));
+                else
+                    return;
             }
+            
+            _viewModels.Add(viewModel);
+            CurrentViewModel = viewModel;
+        }
+
+        private void ExitNavigation()
+        {
+            throw new NotImplementedException();
         }
     }
 }
