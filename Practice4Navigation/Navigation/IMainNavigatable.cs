98fd8751967e97c735a508464e094d4711ba20e0
appleVovan <yablonskyi.v.reg@gmail.com>
2022-02-09 21:53:41 +0200

    MainNavigatable was added

---
+++ Practice4Navigation/Navigation/IMainNavigatable.cs
@@ -0,0 +1,16 @@
+﻿namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.Navigation
+{
+    enum MainNavigationTypes
+    {
+        Main,
+        Auth
+    }
+
+    internal interface IMainNavigatable
+    {
+        MainNavigationTypes ViewType
+        {
+            get;
+        }
+    }
+}
