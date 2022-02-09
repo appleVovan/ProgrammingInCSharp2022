b0119d61958d815a2aff739c9949a1c8832c58f9
appleVovan <yablonskyi.v.reg@gmail.com>
2022-02-09 15:35:15 +0200

    INotifyPropertyChanged was added

--- Practice4Navigation/ViewModels/AuthViewModel.cs
+++ Practice4Navigation/ViewModels/AuthViewModel.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using KMA.ProgrammingInCSharp2022.Practice4Navigation.Annotations;
 using KMA.ProgrammingInCSharp2022.Practice4Navigation.Navigation;
 
 namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.ViewModels
 {
-    class AuthViewModel
+    class AuthViewModel : INotifyPropertyChanged
     {
         private List<IAuthNavigatable> _viewModels = new List<IAuthNavigatable>();
         private Action _exitNavigation;
+        private IAuthNavigatable _currentViewModel;
 
         public IAuthNavigatable CurrentViewModel
         {
-            get;
-            private set;
+            get 
+            {
+                return _currentViewModel;
+            }
+            private set 
+            {
+                if (_currentViewModel == value)
+                    return;
+                _currentViewModel = value;
+                OnPropertyChanged();
+            }
         }
 
         public AuthViewModel(Action exitNavigationAction)
@@ -63,6 +76,14 @@ namespace KMA.ProgrammingInCSharp2022.Practice4Navigation.ViewModels
         private void ExitNavigation()
         {
             _exitNavigation.Invoke();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
